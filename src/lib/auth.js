e44d129ecf6d498ef66caa201b2251f2e58cda33
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-08-27 18:26:39 +0300

    fixes

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -8,7 +8,7 @@ export function getLoggedInUserId() {
   const decodedString = window.atob(middlePart)
   const decodedObj = JSON.parse(decodedString)
   console.log(decodedObj);
-  return decodedObj.userId
+  return decodedObj.sub
 }
 
 
@@ -16,3 +16,4 @@ export function isCreator(userIdToCompare) {
   if (!userIdToCompare) return false
   return userIdToCompare === getLoggedInUserId()
 }
+
